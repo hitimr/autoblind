2671f34837427fc471e387c4482894b1595a94ce
hitimr <hitimr@gmail.com>
2022-02-08 06:25:48 +0100

    74hc via spi works

--- 2_src/esp32/main.py
+++ 2_src/esp32/main.py
@@ -1,5 +1,6 @@
 import json
-import network 
+import network
+import socket
 
 # Connect to WiFi
 sta_if = network.WLAN(network.STA_IF)
@@ -12,12 +13,12 @@ wifi_credentials = json.load(open("wifi_credentials.json"))
 
 print('connecting to network...')
 if not sta_if.isconnected():
-   
-   sta_if.connect(wifi_credentials["ssid"], wifi_credentials["password"])
-   while not sta_if.isconnected():
-         pass
-   print("Connection established")
+
+    sta_if.connect(wifi_credentials["ssid"], wifi_credentials["password"])
+    while not sta_if.isconnected():
+        pass
+    print("Connection established")
 else:
-   print("Already connected")
+    print("Already connected")
 
 print('network config:', sta_if.ifconfig())
